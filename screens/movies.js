6afd809844c9fa90a757013c17dd34f27da66278
unknown <shangeeran@gmail.com>
2020-07-14 22:13:02 +0530

    add Pull to refresh layout

--- screens/movies.js
+++ screens/movies.js
@@ -1,43 +1,96 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import Success from './success';
+//This is an example code for React Native Swipe Down  to Refresh ListView Using RefreshControl//
+import React, { Component } from 'react';
 
-class Movies extends Component {
-    render() {
+import {StyleSheet,ActivityIndicator,FlatList,Text,View,Alert,RefreshControl,
+} from 'react-native';
+//import all the components we are going to use.
+
+export default class Project extends Component {
+    constructor(props) {
+        super(props);
+        //True to show the loader
+        this.state = { refreshing: true };
+        //Running the getData Service for the first time
+        this.GetData();
+    }
+
+    GetData = () => {
+        //Service to get the data from the server to render
+        return fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(responseJson => {
+                this.setState({
+                    refreshing: false,
+                    //Setting the data source for the list to render
+                    dataSource: responseJson
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+    ListViewItemSeparator = () => {
         return (
-            // Logout function
-            <View>
-                <TouchableOpacity style = {styles. buttonContainer}
-                                  onPress={() => this.props.navigation.navigate('Success', { screenName: "Success" })}
-                >
-                    <Text style = {styles.textNext}>
-                        Log out
-                    </Text>
-                </TouchableOpacity>
+            //returning the listview item saparator view
+            <View
+                style={{
+                    height: 0.2,
+                    width: '90%',
+                    backgroundColor: '#808080',
+                }}
+            />
+        );
+    };
+    onRefresh() {
+        //Clear old data of the list
+        this.setState({ dataSource: [] });
+        //Call the Service to get the latest data
+        this.GetData();
+    }
+    render() {
+        if (this.state.refreshing) {
+            return (
+                //loading view while data is loading
+                <View style={{ flex: 1, paddingTop: 20 }}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
+        return (
+            //Returning the ListView
+            <View style={styles.MainContainer}>
+                <FlatList
+                    data={this.state.dataSource}
+                    keyExtractor={(item, index) => index.toString()}
+                    ItemSeparatorComponent={this.ListViewItemSeparator}
+                    enableEmptySections={true}
+                    renderItem={({item}) => (
+                        <Text
+                            style={styles.rowViewContainer}
+                            onPress={() => alert(item.id)}>
+                            {item.title}
+                        </Text>
+                    )}
+                    refreshControl={
+                        <RefreshControl
+                            //refresh control used for the Pull to Refresh
+                            refreshing={this.state.refreshing}
+                            onRefresh={this.onRefresh.bind(this)}
+                        />
+                    }
+                />
             </View>
         );
     }
 }
-
 const styles = StyleSheet.create({
-    buttonContainer : {
-        borderWidth: 1,
-        borderRadius: 30,
-        paddingVertical: 8,
-        paddingHorizontal: 40,
-        borderColor: '#05C167',
-        backgroundColor: '#05C167',
-        marginTop: 20,
-        marginLeft: 260,
+    MainContainer: {
+        justifyContent: 'center',
+        flex: 1,
+        marginTop: 10,
     },
-    textNext: {
-        fontFamily: 'serif',
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#fff',
+    rowViewContainer: {
+        fontSize: 20,
+        padding: 10,
     },
 });
-
-export default Movies;
-
-
